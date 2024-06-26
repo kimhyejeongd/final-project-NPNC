bc13b297ea8b0861bf6341f71dfdc2c9708c7bc2
Kisoo123 <167399086+Kisoo123@users.noreply.github.com>
2024-06-26 12:08:59 +0900

    KS Member DTO 생성 Merge branch 'dev' of https://github.com/JinHyung-dev/NPNC.git into dev

---
+++ src/main/java/com/project/npnc/member/model/dto/Member.java
@@ -0,0 +1,29 @@
+package com.project.npnc.member.model.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+	private int memberKey;
+	private int departmentKey;
+	private int jobKey;
+	private String memberId;
+	private String memberPw;
+	private String memberPhone;
+	private String memberEmail;
+	private String memberMessage;
+	private Date enrollDate;
+	private Date memberLeaveDate;
+	private String memberTell;
+	private String memberAddress;
+	private int accessKey;
+
+}
