92387fb92fb13bde176018911ed3e047a22bf6d4
JinHyung-Park <trupjh@gmail.com>
2024-06-28 17:50:10 +0900

    jh 결재문서 홈 생성, 컨트롤러 생성

---
+++ src/main/java/com/project/npnc/document/member/controller/MemberDocumentController.java
@@ -0,0 +1,13 @@
+package com.project.npnc.document.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/document")
+public class MemberDocumentController {
+	@GetMapping("/home")
+	public void test() {
+	}
+}
