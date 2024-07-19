95e8167b370c7d4fa858ca5e2706374868c494d4
Hyejeong Kim <heajung2665@naver.com>
2024-07-19 09:58:21 +0900

    hj 이메일 전송 구현 완료함

--- src/main/java/com/project/npnc/mypage/controller/MemberController.java
+++ src/main/java/com/project/npnc/mypage/controller/MemberController.java
@@ -54,12 +54,13 @@ public class MemberController {
         message.setTo(email);
         message.setSubject("비밀번호 변경을 위한 인증 코드");
         message.setText("인증 코드: " + generatedCode);
-
+        message.setFrom("heajung2665@naver.com");
         Map<String, Object> response = new HashMap<>();
         try {
             mailSender.send(message);
             response.put("success", true);
         } catch (Exception e) {
+        	System.out.println(e);
             response.put("success", false);
         }
         return response;
