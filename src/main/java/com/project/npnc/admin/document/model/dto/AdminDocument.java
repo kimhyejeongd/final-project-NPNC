1add1ffef53170d71975ab20231bd48bcd70dc29
Kisoo123 <167399086+Kisoo123@users.noreply.github.com>
2024-07-23 17:38:55 +0900

    KS 채팅 연결실패 오류해결, 전자결재 폴더 로직추가

--- src/main/java/com/project/npnc/admin/document/model/dto/AdminDocument.java
+++ src/main/java/com/project/npnc/admin/document/model/dto/AdminDocument.java
@@ -1,5 +1,10 @@
 package com.project.npnc.admin.document.model.dto;
 
+
+import com.project.npnc.admin.department.model.dto.Department;
+import com.project.npnc.admin.job.model.dto.Job;
+import com.project.npnc.security.dto.Member;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +21,8 @@ public class AdminDocument {
     private int erStorageTerm;
     private int erStorageManager;
     private int erStorageOrderby;
+    private Member member;
+    private StorageFolder storageFolder;
+    private Job job;
+    private Department department;
 }
--- src/main/java/com/project/npnc/chatting/controller/HomeController.java
+++ src/main/java/com/project/npnc/chatting/controller/HomeController.java
@@ -83,7 +83,7 @@ public class HomeController {
 //	}
 
 	@GetMapping("chatRoom")
-	public String chatRoom(Model model,HttpSession session,@RequestParam String inputValue) {
+	public String chatRoom(Model model,HttpSession session) {
 		
 
 		Member member = getCurrentUser();
@@ -100,9 +100,20 @@ public class HomeController {
 		List<Member>members = service.selectAllMembers();	
 		model.addAttribute("members",members);
 		
+		System.out.println("mychatRoomListmychatRoomListmychatRoomList"+mychatRoomList);
 		
-
 		return "chatting/chatRoom";
+	}
+	
+	@GetMapping("/")
+	public String headerChatroom(Model model) {
+		Member member = getCurrentUser();
+
+		List<ChattingRoom> mychatRoomList = service.selectMyChatRoomList(member.getMemberKey());
+		model.addAttribute("mychatRoomList",mychatRoomList);
+		System.out.println(mychatRoomList+"========================================");
+		System.out.println("========================================");
+		return "index";
 	}
 	
 	
