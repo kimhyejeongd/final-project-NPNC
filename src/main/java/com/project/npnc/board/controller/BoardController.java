4fffd3b193cc917f1e4eadcf260e3be3eb309eb8
Hyejeong Kim <heajung2665@naver.com>
2024-08-01 23:46:27 +0900

    hj 게시판 끝

--- src/main/java/com/project/npnc/board/controller/BoardController.java
+++ src/main/java/com/project/npnc/board/controller/BoardController.java
@@ -13,10 +13,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.project.npnc.board.model.dto.BoardCommentDto;
@@ -161,4 +161,5 @@ public class BoardController {
         boardService.createComment(commentDto);
         return "redirect:/board/detail/boardKey?boardKey=" + commentDto.getBOARD_KEY();
     }
+    
 }
